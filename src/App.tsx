5e70cb4b9c41c7a3bbe07729685e3dd7304ce650
hishameh <109773901+hishameh@users.noreply.github.com>
2025-11-11 10:51:00 +0300

    Set basename for BrowserRouter in App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,10 @@ import ArchitectFee from './components/ArchitectFee';
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/vanilla-space-sculpt-63019">
       <div className="min-h-screen bg-gray-50">
         <Routes>
+          <Route path="/" element={<ArchitectFee />} />
           <Route path="/architect-fee" element={<ArchitectFee />} />
           {/* other routes */}
         </Routes>
