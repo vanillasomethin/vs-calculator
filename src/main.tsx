21afad38560d64a3a56ae573994a159248490425
hishameh <109773901+hishameh@users.noreply.github.com>
2025-11-15 08:45:33 +0300

    Import global cursor styles in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import './styles/global-cursor.css'  // Add this line
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
